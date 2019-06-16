b6aa3711beebef947cdb8383ceb4ef4a458e3155
Amudski <svalebaek3@gmail.com>
2019-06-16 18:05:37 +0200

    Burgerbar javascript done

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    // toggle nav
+function navSlide() {
+    let burger = document.querySelector('.burger');
+    let nav = document.querySelector('.nav-links');
+    let navLinks = document.querySelectorAll('.nav-links li');
+    // toggler nav bar
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
 
-        //animate links
+        //Animerer links
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = ''
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
                 console.log(index / 7);
             }
-
-
         });
-
-        burger.classList.toggle('toggle');
-
+        burger.classList.toggle('burgerkolaps');
     });
-
 }
-
 navSlide();
